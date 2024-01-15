75c85145a67632c0aaa6405a4c4283c3cc3026d6
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2024-01-15 00:13:08 -0500

    Added more solutions to various problems.

--- Basic Array Transformations/Array Reduce Transformation.js
+++ Basic Array Transformations/Array Reduce Transformation.js
@@ -6,4 +6,26 @@
  *  Date            : January 14, 2024
  **/
 
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    // Edge case
+    if( nums.length == 0 ){
+        return init;
+    }
 
+    // Sequentially execute the reducer function for all elements in the array
+    let tempVal = 0;
+    for( let i = 0; i < nums.length;i++){
+        if( i == 0 ){
+            tempVal = fn(init, nums[i])
+        }else{
+            tempVal = fn(tempVal, nums[i])
+        }
+    }
+    return tempVal
+};
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 30-days-of-javascript
+
+This file contains my solution to the problems inside the 30 Days Of JavaScript study plan on Leetcode. 
+
+Note: Please do not copy my solutions as it will hinder your learning process.
+
